c8deded6bfbcf549140d78c2588bb32140126f9a
l.rojas <luisrojas@radi.pet>
2025-01-15 11:40:04 -0600

    exercises and diet

--- src/app/admin/business/users/diet/diet.component.ts
+++ src/app/admin/business/users/diet/diet.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { format, startOfWeek, addDays } from 'date-fns';
 import { es } from 'date-fns/locale';
+// import { load } from 'gapi-script';
 
 @Component({
   selector: 'app-diet',
@@ -10,7 +11,12 @@ import { es } from 'date-fns/locale';
   styleUrl: './diet.component.scss'
 })
 export class DietComponent {
-
+  private CLIENT_ID = 'TU_CLIENT_ID';
+  private API_KEY = 'TU_API_KEY';
+  private DISCOVERY_DOCS = [
+    'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'
+  ];
+  private SCOPES = 'https://www.googleapis.com/auth/calendar';
 
  
    public diets =  [
@@ -196,7 +202,8 @@ export class DietComponent {
       }
     ]
   }
-]
+   ]
+  
   
    getWeekDays(startDate: Date): string[] {
     const startOfWeekDate = startOfWeek(startDate, { locale: es }); // Inicio de la semana
@@ -214,11 +221,41 @@ export class DietComponent {
   daysOfThisWeek: any = [];
 
   constructor() {
+    // this.initClient();
+
     this.daysOfThisWeek = this.getWeekDays(new Date());
     console.log(this.daysOfThisWeek);
     
   }
   
-  
+  addDietsToCalendar() {
+    console.log('calendario')
+  }
+
+
+
+  //     private initClient() {
+  //   load('client:auth2', () => {
+  //     gapi.client.init({
+  //       apiKey: this.API_KEY,
+  //       clientId: this.CLIENT_ID,
+  //       discoveryDocs: this.DISCOVERY_DOCS,
+  //       scope: this.SCOPES,
+  //     });
+  //   });
+  // }
+
+  // public async signIn() {
+  //   const GoogleAuth = gapi.auth2.getAuthInstance();
+  //   return GoogleAuth.signIn();
+  // }
+
+  // public async createEvent(event: any) {
+  //   const request = gapi.client.calendar.events.insert({
+  //     calendarId: 'primary',
+  //     resource: event,
+  //   });
+  //   return request.execute();
+  // }
   
 }
--- src/app/admin/business/users/exercises/exercises.component.ts
+++ src/app/admin/business/users/exercises/exercises.component.ts
@@ -1,11 +1,289 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { format, startOfWeek, addDays } from 'date-fns';
+import { es } from 'date-fns/locale';
 
 @Component({
   selector: 'app-exercises',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './exercises.component.html',
   styleUrl: './exercises.component.scss'
 })
 export class ExercisesComponent {
+
+  daysOfThisWeek: any = [];
+
+  exercises: any =
+    [
+      {
+        "day": "monday",
+        "exercises": [
+          {
+            "name": "Cardio",
+            "type": "Running",
+            "duration": "30 minutes",
+            "intensity": "Moderate",
+            "calories_burned": 300,
+            "time": "7:00 AM",
+            "notes": "Puedes variar la duración o la intensidad según tu nivel.",
+            "equipment": "Running shoes"
+          },
+          {
+            "name": "Strength Training",
+            "type": "Weight lifting",
+            "duration": "45 minutes",
+            "intensity": "High",
+            "calories_burned": 400,
+            "time": "10:00 AM",
+            "notes": "Enfócate en ejercicios de torso superior.",
+            "equipment": "Dumbbells, Barbell"
+          },
+          {
+            "name": "Stretching",
+            "type": "Yoga",
+            "duration": "20 minutes",
+            "intensity": "Low",
+            "calories_burned": 100,
+            "time": "6:00 PM",
+            "notes": "Ideal para relajarse después de entrenar.",
+            "equipment": "Yoga mat"
+          }
+        ]
+      },
+      {
+        "day": "tuesday",
+        "exercises": [
+          {
+            "name": "Cardio",
+            "type": "Cycling",
+            "duration": "40 minutes",
+            "intensity": "High",
+            "calories_burned": 350,
+            "time": "7:00 AM",
+            "notes": "Asegúrate de mantener una velocidad constante.",
+            "equipment": "Bicycle"
+          },
+          {
+            "name": "Strength Training",
+            "type": "Leg workout",
+            "duration": "40 minutes",
+            "intensity": "Moderate",
+            "calories_burned": 350,
+            "time": "12:00 PM",
+            "notes": "Realiza sentadillas y lunges.",
+            "equipment": "Resistance bands, Weights"
+          },
+          {
+            "name": "Stretching",
+            "type": "Pilates",
+            "duration": "25 minutes",
+            "intensity": "Low",
+            "calories_burned": 120,
+            "time": "6:00 PM",
+            "notes": "Para mejorar flexibilidad y core.",
+            "equipment": "Mat"
+          }
+        ]
+      },
+      {
+        "day": "wednesday",
+        "exercises": [
+          {
+            "name": "Cardio",
+            "type": "Jump Rope",
+            "duration": "15 minutes",
+            "intensity": "High",
+            "calories_burned": 200,
+            "time": "7:00 AM",
+            "notes": "Ideal para un entrenamiento rápido y eficaz.",
+            "equipment": "Jump rope"
+          },
+          {
+            "name": "Strength Training",
+            "type": "Bodyweight exercises",
+            "duration": "30 minutes",
+            "intensity": "Moderate",
+            "calories_burned": 250,
+            "time": "10:00 AM",
+            "notes": "Incluye push-ups, squats, and planks.",
+            "equipment": "None"
+          },
+          {
+            "name": "Stretching",
+            "type": "Stretching routine",
+            "duration": "20 minutes",
+            "intensity": "Low",
+            "calories_burned": 80,
+            "time": "6:00 PM",
+            "notes": "Estira cada grupo muscular por al menos 30 segundos.",
+            "equipment": "None"
+          }
+        ]
+      },
+      {
+        "day": "thursday",
+        "exercises": [
+          {
+            "name": "Cardio",
+            "type": "Swimming",
+            "duration": "30 minutes",
+            "intensity": "Moderate",
+            "calories_burned": 300,
+            "time": "7:00 AM",
+            "notes": "Nada a un ritmo cómodo.",
+            "equipment": "Swimsuit"
+          },
+          {
+            "name": "Strength Training",
+            "type": "Chest and Arms",
+            "duration": "40 minutes",
+            "intensity": "High",
+            "calories_burned": 350,
+            "time": "12:00 PM",
+            "notes": "Realiza ejercicios como flexiones y press de banca.",
+            "equipment": "Dumbbells, Bench"
+          },
+          {
+            "name": "Stretching",
+            "type": "Yoga",
+            "duration": "30 minutes",
+            "intensity": "Low",
+            "calories_burned": 130,
+            "time": "6:00 PM",
+            "notes": "Focalízate en el estiramiento de espalda y piernas.",
+            "equipment": "Yoga mat"
+          }
+        ]
+      },
+      {
+        "day": "friday",
+        "exercises": [
+          {
+            "name": "Cardio",
+            "type": "Running",
+            "duration": "30 minutes",
+            "intensity": "High",
+            "calories_burned": 300,
+            "time": "7:00 AM",
+            "notes": "Sigue un ritmo rápido para mejorar tu resistencia.",
+            "equipment": "Running shoes"
+          },
+          {
+            "name": "Strength Training",
+            "type": "Back workout",
+            "duration": "45 minutes",
+            "intensity": "High",
+            "calories_burned": 400,
+            "time": "12:00 PM",
+            "notes": "Incluye dominadas, remo y deadlifts.",
+            "equipment": "Pull-up bar, Dumbbells"
+          },
+          {
+            "name": "Stretching",
+            "type": "Pilates",
+            "duration": "20 minutes",
+            "intensity": "Low",
+            "calories_burned": 100,
+            "time": "6:00 PM",
+            "notes": "Concentra la rutina en los músculos centrales.",
+            "equipment": "Mat"
+          }
+        ]
+      },
+      {
+        "day": "saturday",
+        "exercises": [
+          {
+            "name": "Cardio",
+            "type": "Running",
+            "duration": "30 minutes",
+            "intensity": "High",
+            "calories_burned": 300,
+            "time": "7:00 AM",
+            "notes": "Sigue un ritmo rápido para mejorar tu resistencia.",
+            "equipment": "Running shoes"
+          },
+          {
+            "name": "Strength Training",
+            "type": "Back workout",
+            "duration": "45 minutes",
+            "intensity": "High",
+            "calories_burned": 400,
+            "time": "12:00 PM",
+            "notes": "Incluye dominadas, remo y deadlifts.",
+            "equipment": "Pull-up bar, Dumbbells"
+          },
+          {
+            "name": "Stretching",
+            "type": "Pilates",
+            "duration": "20 minutes",
+            "intensity": "Low",
+            "calories_burned": 100,
+            "time": "6:00 PM",
+            "notes": "Concentra la rutina en los músculos centrales.",
+            "equipment": "Mat"
+          }
+        ]
+      },
+      {
+        "day": "sunday",
+        "exercises": [
+          {
+            "name": "Cardio",
+            "type": "Running",
+            "duration": "30 minutes",
+            "intensity": "High",
+            "calories_burned": 300,
+            "time": "7:00 AM",
+            "notes": "Sigue un ritmo rápido para mejorar tu resistencia.",
+            "equipment": "Running shoes"
+          },
+          {
+            "name": "Strength Training",
+            "type": "Back workout",
+            "duration": "45 minutes",
+            "intensity": "High",
+            "calories_burned": 400,
+            "time": "12:00 PM",
+            "notes": "Incluye dominadas, remo y deadlifts.",
+            "equipment": "Pull-up bar, Dumbbells"
+          },
+          {
+            "name": "Stretching",
+            "type": "Pilates",
+            "duration": "20 minutes",
+            "intensity": "Low",
+            "calories_burned": 100,
+            "time": "6:00 PM",
+            "notes": "Concentra la rutina en los músculos centrales.",
+            "equipment": "Mat"
+          }
+        ]
+      }
+    ]
+
+
+  constructor() { 
+    this.daysOfThisWeek = this.getWeekDays(new Date());
+  }
+
+  
+  addDietsToCalendar() {
+     console.log('calendario')
+  }
+
+
+  getWeekDays(startDate: Date): string[] {
+      const startOfWeekDate = startOfWeek(startDate, { locale: es }); // Inicio de la semana
+      const weekDays: string[] = [];
+      for (let i = 0; i < 7; i++) {
+        const currentDay = addDays(startOfWeekDate, i);
+        const formattedDate = format(currentDay, "eeee, d 'de' MMMM", { locale: es });
+        weekDays.push(formattedDate);
+      }
+  
+      return weekDays;
+     }
 
 }
